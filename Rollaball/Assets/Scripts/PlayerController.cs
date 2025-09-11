614a4517bbe83324ff346eb4d29ba2eceb0a2ff0
ChrisTuck04 <christopher@tuckman.us>
2025-09-11 17:50:54 -0400

    Completed up to win text of tutorial

--- Rollaball/Assets/Scripts/PlayerController.cs
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -1,29 +1,56 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody rb; 
+    private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent <Rigidbody>(); 
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
-    private void FixedUpdate() 
-   {
-        Vector3 movement = new Vector3 (movementX, 0.0f, movementY); 
-        rb.AddForce(movement * speed); 
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        Debug.Log("Movement Vector: " + movementVector);
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 11)
+        {
+            winTextObject.SetActive(true);
+        }
    }
 
-    void OnMove (InputValue movementValue)
+    void OnTriggerEnter(Collider other)
     {
-        Vector2 movementVector = movementValue.Get<Vector2>(); 
-        Debug.Log("Movement Vector: " + movementVector);
-        movementX = movementVector.x; 
-        movementY = movementVector.y; 
-    }
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+   }
 }
